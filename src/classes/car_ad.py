02128e9d37eef1ed34b1b788b6c679790434db7e
InWamos <mykyta.pidgornyi@gmail.com>
2023-03-04 11:19:09 +0200

    rewritten for a new parsing module

---
+++ src/classes/car_ad.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
+class CarAdvertisement:
+    """Dataclass. Consists of 3 variables, used to provide a return of
+    get_last_advertisement()
+
+    Returns:
+        LastOffer: consists of
+            link to the page with advertisements(link_to_page),
+            link to certain advertisement(link_to_item),
+            name of the advertisement(name);
+    """
+    link_to_page: str
+    link_to_item: str
+    name: str
+
+    def __str__(self) -> str:
+        return f"Новый автомобиль!\n🚘 Название: {self.name}\n🔗 Ссылка: {self.link_to_item}"
+  
